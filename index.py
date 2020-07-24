9d85cb899cc2feb4a753f61c41a4b3d8c996d51b
Anmol Agarwal <anmolagarwal4453@gmail.com>
2020-07-24 07:36:04 +0530

    an early less effective version of importing the data set

---
+++ index.py
@@ -0,0 +1,88 @@
+# Here "flask" is the package name
+# "Flask" is a class present im the package
+from flask import Flask
+
+# render_template is a function we are importing from the flask package
+from flask import render_template
+
+#############################################################
+# importing stuff
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import nltk
+########################################
+
+# "app" is an object of class "Flask"
+'''__name__ is the name of the current Python module. The app needs to know where it’s located to set up some paths, 
+and __name__ is a convenient way to tell it that'''
+app = Flask(__name__)
+
+#################################################################
+# specifying the excel file to be used
+excel_file = "Data_Train (1).xlsx"
+cars = pd.read_excel(excel_file)
+
+# Setting all columns to lower case
+cars.columns = [x.lower() for x in cars.columns]
+cars = cars.rename(columns={"location": "city", "kilometers_driven": "kms", "fuel_type": "fuel",
+                            "owner_type": "owner", })
+
+
+a1 = []
+a2 = []
+for s in cars["name"]:
+    ary = s.split(' ')
+    a1.append(ary[0])
+    a2.append(ary[1])
+
+brand = pd.Series(a1)
+model = pd.Series(a2)
+
+cars["brand"] = brand
+cars["model"] = model
+cars.loc[cars["brand"] == "Land", "brand"] = "Land_Rover"
+cars.loc[cars["brand"] == "Isuzu", "brand"] = "ISUZU"
+
+arr = list(brand.unique())
+arr.remove("Isuzu")
+arr2 = list(model.unique())
+
+info = {}
+for i in range(len(cars)):
+    brand_curr = cars["brand"][i]
+    model_curr = cars["model"][i]
+    info[model_curr] = brand_curr
+
+
+#############################################################
+
+
+# @app.route() creates a simple route
+@app.route('/')
+def index():
+    return "<h1>This is basic.py file</h1>"
+
+
+@app.route('/predict')
+def get_data():
+    pass
+
+
+@app.route('/information')
+def info():
+    return "<h1>Vought International is evil corp</h1>"
+# ///////////////////////////////////////////
+
+
+@app.route('/hero/<name>')
+def hero(name):
+    # print(name)
+    return f"<h1>The name of the hero is {name}</h1>"
+
+
+if __name__ == "__main__":
+    # debug - True helps to make the real changes in code visible in the browser
+    # debug -True job is to display debugging pages if an error occurs
+    app.run(debug=True)
